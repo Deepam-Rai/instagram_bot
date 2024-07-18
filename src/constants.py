bcc83ab4e70a317dc2a56266ab2d48a00a741782
Deepam-Rai <deepmrai1@gmail.com>
2024-07-18 23:58:37 +0530

    Added Symnola font for emoji support.

--- src/constants.py
+++ src/constants.py
@@ -2,7 +2,7 @@ VERT_OFFSET = 0.10  # static Constant
 CONTRAST_FACTOR = 0.5  # contrast multiplier
 PARA_PADDING = 10
 PARA_WIDTH = 40
-DEFAULT_FONT = 'IBMPlexSerif-Regular.ttf'
+DEFAULT_FONT = 'Symbola.ttf'
 FONT_COLOR = "black"
 FONT_SIZE = 48
 
