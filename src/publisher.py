2ae8e277bfce6d5fb9f0b655ffc1e86b877faea2
Deepam-Rai <deepmrai1@gmail.com>
2024-08-01 14:44:37 +0530

    Optimization.

--- src/publisher.py
+++ src/publisher.py
@@ -8,12 +8,20 @@ logger = logging.getLogger(__name__)
 
 class InstaPublisher:
     def __init__(self, insta_creds: dict):
-        self.client = Client()
+        self.creds = insta_creds
+        self.client: Client = None
+
+    def login(self) -> None:
         logger.info("Logging into instagram...")
-        self.client.login(insta_creds["username"], insta_creds["password"])
+        self.client = Client()
+        self.client.login(self.creds["username"], self.creds["password"])
 
     def publish_photo(self, path: Path, caption: str = None):
+        if self.client is None:
+            self.login()
         self.client.photo_upload(path=path, caption=caption)
 
     def publish_album(self, paths: list[Path], caption: str = None):
+        if self.client is None:
+            self.login()
         self.client.album_upload(paths=paths, caption=caption)
